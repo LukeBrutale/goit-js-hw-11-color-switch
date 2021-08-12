51069e2f856409802078b55c93438a28d756592a
LukeBrutale <yuriybema@gmail.com>
2021-08-12 10:50:54 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,14 @@ const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'
 const startBtn = document.querySelector('[data-action="start"]');
 const stopBtn = document.querySelector('[data-action="stop"]');
 
-//Для генерации случайного числа (индекс элемента массива цветов)
+//Для генерации случайного числа 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-document.body.style.backgroundColor = colors[2];
+document.body.style.backgroundColor = colors[0];
 
-let timlerId = null;
+let switchId = null;
 let isActive = false;
 
 startBtn.addEventListener('click', onClickStartBtn);
@@ -21,13 +21,13 @@ function onClickStartBtn(event) {
     return;
   }
   isActive = true;
-  timlerId = setInterval(() => {
-    console.log('hello');
+  switchId = setInterval(() => {
+    console.log('say hello:)');
     document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
   }, 1000);
 }
 
 function onClickStopBtn(event) {
   isActive = false;
-  clearInterval(timlerId);
+  clearInterval(switchId);
 }
